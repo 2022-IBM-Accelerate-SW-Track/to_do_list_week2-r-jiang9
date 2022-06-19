e08d7c8bd1abc0bf6c496aa2c8de46526ce3eb1d
Renee Jiang <reneejiang@Renees-MBP.fios-router.home>
2022-06-19 19:59:00 -0400

    dates added

--- src/component/todos.js
+++ src/component/todos.js
@@ -14,12 +14,12 @@ const Todos = ({ todos , deleteTodo }) => {
     todos.map((todo) => {
       return (
         <Grid key={todo.id}>
-          <Card>
+          <Card style={{marginTop:10}}>
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
              <ListItemButton component="a" href="#simple-list">
-               <Checkbox style = {{paddingLeft:0}, {color: "primary"}} onClick={() => deleteTodo(todo.id)} />
-                    <ListItemText primary={todo.content}/>
+                     <Checkbox style= {{paddingLeft:0}} color= "primary" onClick={() => deleteTodo(todo.id)} />
+                    <ListItemText primary={todo.content} secondary={todo.date}/>
               </ListItemButton>
           </Card>
         </Grid>
